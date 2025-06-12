6188897e2db42cbdd775b21081d2bf618bade3f5
kipson2805 <kipssmilly@gmail.com>
2025-06-12 12:23:35 +0530

    addition

--- C2TC_CodeJavaPrograms/src/day1/SampleJava.java
+++ C2TC_CodeJavaPrograms/src/day1/SampleJava.java
@@ -1,9 +1,19 @@
 package day1;
 
+import java.util.Scanner;
+
+
 public class SampleJava {
 
 	public static void main(String[] args) {
-	System.out.println("welcome to the java");
+	  Scanner sc = new Scanner(System.in);
+	  System.out.println("enter a value");
+	  int a = sc.nextInt();
+	  System.out.println("enter b value");
+	  int b = sc.nextInt();
+	  int c=a+b;
+	  System.out.println("your ans is : " + c);
+			
 
 	}
 
